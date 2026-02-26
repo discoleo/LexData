8b2562af346be46ffac4a22362706936d272ace0
discoleo <lmada@umft.ro>
2026-02-26 21:13:16 +0200

    Updated List of Sheets;
    + some tools;

--- Tools.Lex.R
+++ Tools.Lex.R
@@ -6,7 +6,9 @@ library(readODS)
 
 # Read Words from ODS-file
 import.words = function(file = "Words.Med.SARS.ods", verbose = TRUE) {
-	sheets = c("Nouns", "Drugs", "Chem", "Other", "Abbr", "Names");
+	sheets = c("Nouns", "Drugs", "Chem", "Other", "Abbr",
+		"Genes", "CellLines", "Names"
+		);
 	x = c();
 	for(sh in sheets) {
 		ww = read_ods(file, sheet = sh, col_names = TRUE, as_tibble = FALSE);
@@ -32,5 +34,24 @@ write.words = function(x, file = "Words.csv") {
 	}
 	write.csv(x, file = file, row.names = FALSE);
 	invisible();
+}
+
+### UTF Codes:
+which.utf = function(x) {
+	stringi::stri_escape_unicode(x);
+}
+
+### Trim Abstracts
+# - remove Extra-Info;
+trim.extra = function(x) {
+	# \\u00a9: (C)
+	reg = paste("\u00a9", "Copyright",
+		"This journal is \u00a9", "Communicated by ", sep = "|");
+	reg = paste0("[.] ?(?:", reg, ")");
+	npos = regexpr(reg, x$Abstract);
+	isSubS = npos > 1;
+	npos = npos[isSubS];
+	x$Abstract[isSubS] = substr(x$Abstract[isSubS], 1, npos);
+	invisible(x);
 }
 
